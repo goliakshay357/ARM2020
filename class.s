b81684afb9f5e5a7e797c2c55500ba51755eb8e0
Goli Akshay Sujith <32712438+goliakshay357@users.noreply.github.com>
2020-10-01 16:30:10 +0530

    Update class.s

--- class.s
+++ class.s
@@ -7,11 +7,11 @@ __main FUNCTION
 	  MOV r2,#0x21           ;			
 	  MOV r7,#0x10           ;
 	  CMP r2,r7           ;
-	  ITTEE LE
-	  SUBLE r2,r2,r7           ;r2 = r2 − r7;
-	  MOVLE r2,#0x0           ;r2 = 0 ;
-	  ADDGT r2,r2,r4           ;r2 = r2 + r4;
-	  MOVGT r4,#0x0           ;r4 = 0
+	  ITTEE NE
+	  SUBNE r2,r2,r7           ;r2 = r2 − r7
+	  MOVNE r2,#0x0           ;r2 = 0 
+	  ADDEQ r2,r2,r4           ;r2 = r2 + r4
+	  MOVEQ r4,#0x0           ;r4 = 0
    
    ENDFUNC
    END
